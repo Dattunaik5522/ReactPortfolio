b381346d075cfac79ca419609e2cc17cb1acbf86
Dattunaik5252 <gugulothdattu000@gmail.com>
2023-09-01 12:31:59 +0530

    Done with contact experienc eportifolio components

--- portifolioapp/src/App.js
+++ portifolioapp/src/App.js
@@ -1,6 +1,9 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
+import Experience from "./components/Experience";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
+import Portifolio from "./components/Portifolio";
 import SocialLinks from "./components/SocialLinks";
 
 function App() {
@@ -9,6 +12,9 @@ function App() {
       <NavBar />
       <Home />
       <About />
+      <Portifolio />
+      <Experience />
+      <Contact />
       <SocialLinks />
     </div>
   );
